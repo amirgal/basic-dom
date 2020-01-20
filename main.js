2eb549637a83b86325bb19a68bd12068ac10f4cd
amirgal <amirc538@gmail.com>
2020-01-20 11:37:25 +0200

    added moveRight function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// console.log(document)
+
+const moveRight = function(){
+    let block = document.getElementById("block")
+    let left = parseInt(block.style.left) || 0    // will parse a number, or return 0 if it can't parse
+    left = left +15
+    block.style.left = left+'px'
+  }
+  
